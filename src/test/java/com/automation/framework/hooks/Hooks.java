717d45b51fd97add9817bd55d38079c6eb10bcbc
Aytan Mahammadova <aytan.mahammadova@mss-msp.local>
2026-03-17 17:47:34 +0000

    Added config reader

--- src/test/java/com/automation/framework/hooks/Hooks.java
+++ src/test/java/com/automation/framework/hooks/Hooks.java
@@ -2,12 +2,33 @@
 package com.automation.framework.hooks;
 
 import com.automation.framework.base.DriverFactory;
+import com.automation.framework.pages.ServiceNowAuthenticationPage;
+
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import io.cucumber.java.After;
+import io.cucumber.java.Before;
 import io.cucumber.java.Scenario;
+import com.automation.framework.utilities.ConfigReader;
 
 public class Hooks {
+
+    @Before("@login")
+    public void setUp() {
+
+        // 1. Launch browser + navigate to URL
+        DriverFactory.createDriver();
+        DriverFactory.getDriver().get(
+                ConfigReader.getProperty("auth.url"));
+
+        // 2. Initialize authentication page
+        ServiceNowAuthenticationPage authenticationPage = new ServiceNowAuthenticationPage(DriverFactory.getDriver());
+
+        // 3. Perform login
+        authenticationPage.login(
+                ConfigReader.getProperty("auth.username"),
+                ConfigReader.getProperty("auth.password"));
+    }
 
     @After
     public void tearDown(Scenario scenario) {
--- src/test/resources/features/servicenow_incident_navigation.feature
+++ src/test/resources/features/servicenow_incident_navigation.feature
@@ -2,14 +2,12 @@
 @allure.label.parentSuite:ServiceNow
 Feature: Incident Navigation
 
-@servicenow_auth
+@login
+
 @incident
 Scenario: ServiceNow Incident Navigation
 
-Given I open the browser for authentication
-When I navigate to authentication url "https://gditsharedtest.servicenowservices.com"
-And I enter authentication username "username" and password "password"
-Then I should be authenticated
+
 When I navigate to incident page
 And I enter incident values
 And I close the incident
